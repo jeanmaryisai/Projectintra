4763d6ebf8e2f531d60721a26aed488a283a1c17
isaijeanmary <jeanmaryisai@gmail.com>
2022-04-04 13:50:11 -0400

    h

--- src/com/company/Menus.java
+++ src/com/company/Menus.java
@@ -19,7 +19,7 @@ public  abstract class Menus {
         do {
             switch (mainMenu()){
                 case 1:moduleEtudiant();break;
-                case 2:test(2);break;
+                case 2:modulePret();break;
                 case 3:test(3);break;
                 case 4:test(4);break;
                 case 5:d("Merci et a bientot");stay=false;
@@ -58,18 +58,16 @@ public  abstract class Menus {
                 d("Presser 1 pour placer un pret\n" +
                         "presser 2 pour afficher tous les prets\n" +
                         "presser 3 pour afficher un prets en details\n" +
-                        "presser 4 pour modifier un client\n" +
-                        "presser 5 pour retourner au menu principale"
+                        "presser 4 pour retourner au menu principale"
                 );
                 choix = ei();
-            } while (choix != 1 && choix != 2 && choix != 3 && choix != 4&&choix!=5);
+            } while (choix != 1 && choix != 2 && choix != 3 && choix != 4);
 
             switch (choix){
-                case 1:createEtudiant();break;
-                case 2:searchStudent();break;
-                case 3:showStudents();break;
-                case 5:stay=false;break;
-                case 4:modifyStudent();break;
+                case 1:addPret();break;
+                case 2:showPrets();break;
+                case 3:displayPret();break;
+                case 4:stay=false;break;
             }
             if(stay)br();
         } while (stay);
